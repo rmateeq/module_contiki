3f2f44fae85cb56864604bfef6d14f5796d9c189
Peter Ruckebusch <peter.ruckebusch@hotmail.com>
2017-03-06 16:06:33 +0100

    fixed bug in merge

--- wishful_module_gitar/lib_sensor.py
+++ wishful_module_gitar/lib_sensor.py
@@ -351,8 +351,8 @@ class SensorNodeFactory():
                 for i, cooja_dev in enumerate(cooja_devs):
                     platform_class = "RM090"
                     platform_module = "lib_msp430"
-
-                    com_wrapper = CoAPWrapper(i + 1, cooja_dev, "115200", "500") #Jan: 500 serial delay for taisc (writing to serial while in interrupt causes issues)                    platform = SensorPlatform.create_instance(platform_module, platform_class)
+                    com_wrapper = CoAPWrapper(i + 1, cooja_dev, "115200", "500")  # Jan: 500 serial delay for taisc (writing to serial while in interrupt causes issues)
+                    platform = SensorPlatform.create_instance(platform_module, platform_class)
                     interface = "lowpan" + str(i)
                     self.__nodes[interface] = RPCNode(interface, platform, com_wrapper)
             except subprocess.CalledProcessError:
@@ -384,7 +384,6 @@ class SensorNodeFactory():
                     platform_class = "RM090"
                     platform_module = "lib_msp430"
                     self.log.info("Found RM090 on %s", mote_dev)
-                    com_wrapper = CoAPWrapper(mote_dev_id, mote_dev, "115200")
                     com_wrapper = CoAPWrapper(mote_dev_id, mote_dev, "115200", "500")
                 else:
                     self.log.info("skipping unknown node type")
